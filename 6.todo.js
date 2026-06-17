0bdae1eb61d104749bc22fd676742f4ae4d4bc76
asm-saim <saim777m@gmail.com>
2026-06-17 14:27:06 +0600

    applied async, await in todo

--- 6.todo.js
+++ 6.todo.js
@@ -1,29 +1,38 @@
-const allTodo = () => {
+const allTodo = async () => {
+
     const url = "https://jsonplaceholder.typicode.com/todos";
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => todoCard(data));
+    // 1. Fetch data (wait for response)
+    const response = await fetch(url);
+    // 2. Convert to JSON (wait for parsing)
+    const data = await response.json();
 
-    // 1. get the container, empty that.
-    const getTodos = document.getElementById("all-todo")
-    getTodos.innerHTML = ''
+    // 3. Get container and clear it
+    const getTodos = document.getElementById("all-todo");
+    getTodos.innerHTML = '';
 
+    // 4. Render UI
     function todoCard(values) {
         values.forEach(value => {
             const todoBox = document.createElement("div");
-            todoBox.innerHTML = `
-            <div class="todoBox">
-                <p>${value.completed == true ?
-                    `<i class="fa-solid fa-square-check"></i>` :
-                    `<i class="fa-regular fa-square-check"></i>`
-                }</p>
-            <h1>${value.title}</h1>  
-            </div>
-            `
-            getTodos.appendChild(todoBox)
-        })
-    }
 
+            todoBox.innerHTML = `
+                <div class="todoBox">
+                    <p>
+                        ${value.completed
+                    ? `<i class="fa-solid fa-square-check"></i>`
+                    : `<i class="fa-regular fa-square-check"></i>`
+                }
+                    </p>
+                    <h1>${value.title}</h1>
+                </div>
+            `;
+
+            getTodos.appendChild(todoBox);
+        });
+    }
+    //Call render
+    todoCard(data);
 };
+
 allTodo();
